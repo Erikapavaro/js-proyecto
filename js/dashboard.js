35b0e8035508b99aa2eafe5820e72e1c27c23dae
erikapavaro <erikapavaro@gmail.com>
2022-07-05 14:29:12 -0500

    correcion de errores

---
+++ js/dashboard.js
@@ -0,0 +1,26 @@
+var menu = document.querySelector('.AppList');
+    var elem1 = document.querySelector('.icon-menu');
+    var elem2 = document.querySelector('.close');
+
+    function openMenu() {
+        menu.style.transition = ".4s"
+        menu.style.width = "30%"
+    }
+
+    function closeMenu() {
+        menu.style.transition = ".4s"
+        menu.style.width = "0%"
+    }
+
+    closeMenu()
+
+    elem1.addEventListener("click",function(){
+        menu.style.transition = ".4s"
+        menu.style.width = "30%"
+    });
+
+    elem2.addEventListener("click", function(){
+        menu.style.transition = ".4s"
+        menu.style.width = "0%"
+
+    });
