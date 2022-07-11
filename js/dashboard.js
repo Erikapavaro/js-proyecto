6f59847186f068a23d4d791bd9aa37c8fe12a1f9
erikapavaro <erikapavaro@gmail.com>
2022-07-11 16:29:56 -0500

    correcion de errores y cambios en html

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,12 +4,20 @@ var contMenu = document.querySelector('.AppList');
 var openMenu = document.querySelector('.icono-menu');
 var closeMenu = document.querySelector('.closer');
 
+
 openMenu.addEventListener('click',function(){
+    /* menu.classList.remove('close-content'); */
+    /* contMenu.classList.add('close-applist') */
+    contMenu.style.width="30%"
     menu.classList.remove('close-content');
-    contMenu.classList.add('close-applist')
 });
 
 closeMenu.addEventListener('click',function(){
+    contMenu.style.width="0%"
     menu.classList.add('close-content');
-    contMenu.classList.add('close-applist')
+});
+
+menu.addEventListener('click',function(){
+    contMenu.style.width="0%"
+    menu.classList.add('close-content');
 });
